69b0de78966adbf2df2d89ff6460e36421f14df4
Partha Sarathi Hazra <sarathiparthahazra9609@gmail.com>
2022-05-29 20:50:50 +0530

    Text To Voice Converter

--- project17.py
+++ project17.py
@@ -1,10 +1,13 @@
 # Text To Voice Converter
 from gtts import gTTS
+import os
 
-text ="I Am Partha Sarathi Hazra"
+abc = open('sample.txt')
+text = abc.read()
 
 language = 'en'
 
 obj = gTTS(text=text,lang=language,slow=False)
-
 obj.save("sample.mp3")
+
+os.system("sample.mp3")
